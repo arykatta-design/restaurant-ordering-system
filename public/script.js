9accdb7f047abbfd09cd78d2c77c48c698813035
Arya Katta <aryakatta@Aryas-MacBook-Air.local>
2026-06-04 23:44:44 +0530

    Fixed script

--- public/script.js
+++ public/script.js
@@ -1,4 +1,108 @@
+let cart = []
+
+fetch("/menu")
+.then(res => res.json())
+.then(menu => {
+
+    const menuDiv = document.getElementById("menu")
+
+    menu.forEach(item => {
+
+        menuDiv.innerHTML += `
+            <div class="card">
+                <img src="${item.image}" width="250">
+                <h2>${item.name}</h2>
+                <p>₹${item.price}</p>
+
+                <button onclick='addToCart(${JSON.stringify(item)})'>
+                    Add To Cart
+                </button>
+            </div>
+        `
+    })
+})
+
+function addToCart(item) {
+
+    const existingItem = cart.find(
+        cartItem => cartItem.id === item.id
+    )
+
+    if(existingItem){
+        existingItem.quantity++
+    }
+    else{
+        cart.push({
+            ...item,
+            quantity: 1
+        })
+    }
+
+    renderCart()
+}
+
+function renderCart() {
+
+    const cartDiv = document.getElementById("cart")
+
+    cartDiv.innerHTML = ""
+
+    let total = 0
+
+    cart.forEach((item, index) => {
+
+        const itemTotal =
+            item.price * item.quantity
+
+        total += itemTotal
+
+        cartDiv.innerHTML += `
+            <p>
+
+                ${item.name}
+                x${item.quantity}
+                - ₹${itemTotal}
+
+                <button onclick="decreaseQty(${index})">
+                    -
+                </button>
+
+                <button onclick="increaseQty(${index})">
+                    +
+                </button>
+
+            </p>
+        `
+    })
+
+    document.getElementById("total").innerText =
+        `Total: ₹${total}`
+}
+
+function increaseQty(index) {
+
+    cart[index].quantity++
+
+    renderCart()
+}
+
+function decreaseQty(index) {
+
+    cart[index].quantity--
+
+    if(cart[index].quantity <= 0){
+        cart.splice(index, 1)
+    }
+
+    renderCart()
+}
+
 function placeOrder() {
+
+    if(cart.length === 0){
+        alert("Cart is empty")
+        return
+    }
 
     const params =
         new URLSearchParams(window.location.search)
@@ -25,7 +129,7 @@ function placeOrder() {
         })
     })
     .then(res => res.json())
-    .then(data => {
+    .then(() => {
 
         cart = []
 
